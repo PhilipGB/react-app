a4aedee8716f49593019d7bf598c86ff2ff507f7
BigYoSpeck <philipgrahamburgess@gmail.com>
2021-08-08 17:16:59 +0100

    basic landing page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>First React App</h1>
+      <h1>About Me</h1>
+      <p class="card-text">
+        My name is Philip Burgess and I am a programmer from Leeds in the United Kingdom. 
+        This portfolio site was built using the Bootstrap and React frameworks.   
+      </p>
     </div>
   );
 }
