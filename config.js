c778ec95a29d1b8870f7b76bef90ec18103e073e
BigYoSpeck <philipgrahamburgess@gmail.com>
2021-08-08 23:19:33 +0100

    test

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'Philip Burgess Portfolio', // Required
+  author: 'Philip Burgess', // Required
+  description: 'Developer Portfolio',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://philipburgess.dev',
+    github: 'https://github.com/ryanfitzgerald',
+  },
+  siteUrl: 'https://philipburgess.dev',
+};
---
+++ src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import userConfig from '../../../config';
+
+import Container from '../Container';
+import HeaderImage from '../HeaderImage';
+import Social from '../Social';
+import H1 from '../H1';
+import P from './P';
+import Link from './Link';
+import Wrapper from './Wrapper';
+
+function Header({ config }) {
+  const { author, description, social } = config;
+
+  return (
+    <Container>
+      <Wrapper>
+        {userConfig.showHeaderImage && (
+          <HeaderImage/>
+        )}
+        <H1><Link to="/">{author}</Link></H1>
+        <P>{description}</P>
+        {social &&
+          <Social
+            website={social.website}
+            github={social.github}
+          />
+        }
+      </Wrapper>
+    </Container> 
+  );
+}
+
+export default Header;
