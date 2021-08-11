283d7dea80dafaa86dae0e7d68ee9a76a8b95b18
BigYoSpeck <philipgrahamburgess@gmail.com>
2021-08-11 17:16:48 +0100

    contact yellow text adjusted

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,7 +7,7 @@ function Contact() {
       <div className="container px-5 py-10 mx-auto text-center lg:px-40">
         <div className="flex flex-col w-full mb-20">
           <UserIcon className="mx-auto inline-block w-10 mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-green-400">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-4 text-yellow-200">
             Contact Me
           </h1>
         </div>
