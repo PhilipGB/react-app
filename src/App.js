65568497c9f06a8ad9859f10a720f990e077ee58
BigYoSpeck <philipgrahamburgess@gmail.com>
2021-08-17 16:06:32 +0100

    Dynamic page attempt

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from 'react';
 import Nav from './components/Nav.js'
 import About from './components/About.js'
 import Projects from './components/Projects.js'
@@ -6,13 +6,22 @@ import Skills from './components/Skills.js'
 import Contact from './components/Contact.js'
 
 function App() {
+  const [pages, setCart] = useState([]);
+  //setCart(cart => [...cart, "First Load"]);
+
+  function reorderPage(e) {
+    const item = e.target;
+    setCart(pages => [<About />, ...pages]);
+  }
+
   return (
     <main className="text-gray-400 bg-gray-800 body-font font-mono">
-      <Nav />
-      <About />
-      <Projects />
-      <Skills />
-      <Contact />
+        <Nav />
+
+        <button value={<About />} onClick={reorderPage}>Test Button</button>
+        {pages.map(page => page)}
+
+        <Projects />
     </main>
   );
 }
