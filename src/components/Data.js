cc2a0331681e1dafb54f9ecc908733bdd01567a2
Philip Burgess <philipgrahamburgess@gmail.com>
2022-01-20 14:04:09 +0000

    news api project added, skills revised

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,33 +1,40 @@
 export const projects = [
-    {
-      title: "CS50 Problem Set 9",
-      subtitle: "Finance",
-      description:
-        "This is my implementation of the CS50 Problem Set 9, Finance running on PythonAnywhere",
-      image: "./images/cs50pset9.gif",
-      link: "https://bigyospeck.pythonanywhere.com/login",
-      github: "https://github.com/BigYoSpeck/CS50Finance",
-    },
-    {
-      title: "React and tailwindcss based portfolio site",
-      subtitle: "Portfolio",
-      description:
-        "This is my React and tailwindcss based developer portfolio site",
-      image: "./images/portfolio.gif",
-      link: "https://github.com/BigYoSpeck/react-app",
-      github: "https://github.com/BigYoSpeck/react-app",
-    },
-    {
-      title: "Future Project Placeholder",
-      subtitle: "TODO",
-      description:
-        "This is the placeholder for a future project to be added to the site soon (Image from unsplash.com)",
-      image: "./images/todo.jpg",
-      link: "https://github.com/BigYoSpeck/",
-      github: "https://github.com/BigYoSpeck/",
-    }
-  ];
+  {
+    title: "CS50 Problem Set 9",
+    subtitle: "Finance",
+    description:
+      "This is my implementation of the CS50 Problem Set 9, Finance running on PythonAnywhere",
+    image: "./images/cs50pset9.gif",
+    link: "https://bigyospeck.pythonanywhere.com/login",
+    github: "https://github.com/BigYoSpeck/CS50Finance",
+  },
+  {
+    title: "React and tailwindcss based portfolio site",
+    subtitle: "Portfolio",
+    description:
+      "This is my React and tailwindcss based developer portfolio site",
+    image: "./images/portfolio.gif",
+    link: "https://github.com/BigYoSpeck/react-app",
+    github: "https://github.com/BigYoSpeck/react-app",
+  },
+  {
+    title: "BigYoSpeck News",
+    subtitle: "News API",
+    description:
+      "Big Yo Speck News is a Node.js/Express based news API providing articles and commenting in JSON format using a PostgreSQL database",
+    image: "./images/todo.jpg",
+    link: "https://bys-news.herokuapp.com/api",
+    github: "https://github.com/BigYoSpeck/bys-news",
+  },
+];
 
-  export const skills = [
-    "HTML", "CSS", "JavaScript", "React", "netlify", "Python", "Flask", "SQL", "Java", "C", "Linux", "Git"
-  ];
+export const skills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Node.js",
+  "SQL",
+  "Linux",
+  "Git",
+];
